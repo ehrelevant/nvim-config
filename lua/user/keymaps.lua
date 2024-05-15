2be84a9b0fef5cb425d427b7ef92f16ca064e86c
Ehren Castillo <ehrencastillo@gmail.com>
2024-05-16 00:42:08 +0800

    feat(keymaps.lua): add more keymaps

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -3,15 +3,49 @@ local defaults = { noremap = true, silent = true }
 -- Leader Key
 vim.g.mapleader = ' '
 
--- Clipboard Keymaps
---[[ 
-vim.keymap.set({'n', 'x'}, '<leader>y', '"+y', defaults) -- Yank Motion
-vim.keymap.set({'n', 'x'}, '<leader>Y', '"+Y', defaults) -- Yank Line
+-- Clipboard Keymaps to register "+"
+vim.keymap.set({'n', 'x'}, '<leader>y', '"+y<CR>', defaults) -- Yank Motion
+vim.keymap.set({'n', 'x'}, '<leader>Y', '"+Y<CR>', defaults) -- Yank Line
 
-vim.keymap.set({'n', 'x'}, '<leader>x', '"+d', defaults) -- Cut Motion
-vim.keymap.set({'n', 'x'}, '<leader>X', '"+D', defaults) -- Cut Line
+vim.keymap.set({'n', 'x'}, '<leader>x', '"+d<CR>', defaults) -- Cut Motion
+vim.keymap.set({'n', 'x'}, '<leader>X', '"+D<CR>', defaults) -- Cut Line
 
-vim.keymap.set({'n', 'x'}, '<leader>p', '"+p', defaults) -- Put Motion
---]]
+vim.keymap.set({'n', 'x'}, '<leader>p', '"+p<CR>', defaults) -- Put Motion
+vim.keymap.set({'n', 'x'}, '<leader>P', '"+P<CR>', defaults) -- Put Motion
 
-vim.keymap.set({'i'}, 'jk', '<ESC>')
+-- map c and d (deleting motions) to blackhole registers
+vim.keymap.set('n', 'd', '"_d', {})
+vim.keymap.set('n', 'c', '"_c', {})
+
+-- map dx to cutting (normal functionality of dd)
+vim.keymap.set('n', 'dx', 'dd', defaults)
+
+-- map jj to esc 
+vim.keymap.set({'i', 'x'}, 'jj', '<ESC>', defaults)
+
+-- auto add closing (, [, {, ', ", <
+-- vim.keymap.set('i', '(<cr>', '(<cr>)<ESC>kA<CR>', {})
+-- vim.keymap.set('i', '[<cr>', '[<cr>]<ESC>kA<CR>', {})
+vim.keymap.set('i', '{<cr>', '{<cr>}<ESC>kA<CR>', {})
+opening_pairs = {'(', '[', '{', "'", '"', '<'}
+closing_pairs = {')', ']', '}', "'", '"', '>'}
+for key, chr in pairs(opening_pairs)
+do
+	vim.keymap.set('i', chr, chr..closing_pairs[key]..'<esc>i', {})
+end
+
+-- Alt + up/down to move line up/down
+vim.keymap.set('n', '<M-Up>', 'yyddkP', defaults)
+vim.keymap.set('n', '<M-Down>', 'yyddp', defaults)
+
+-- Shift + up/down to copy line up/down
+vim.keymap.set('n', '<S-Up>', 'yyP', defaults)
+vim.keymap.set('n', '<S-Down>', 'yyp', defaults)
+
+-- use U for redo
+vim.keymap.set('n', 'U', '<C-r>', {})
+
+-- Insert newline without entering insert mode
+vim.keymap.set('n', '<leader>o', ':<C-u>call append(line("."), repeat([""], v:count1))<CR>', defaults)
+vim.keymap.set('n', '<leader>O', ':<C-u>call append(line(".")-1, repeat([""], v:count1))<CR>', defaults)
+vim.keymap.set('n', '<leader><CR>', 'o<ESC>', defaults)

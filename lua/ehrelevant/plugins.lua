b3409f2328b16c560af940a48c69db6e886c1caa
Ehren Castillo <ehrencastillo@gmail.com>
2024-09-30 17:25:17 +0800

    feat: add toggleterm.nvim

--- lua/ehrelevant/plugins.lua
+++ lua/ehrelevant/plugins.lua
@@ -288,5 +288,20 @@ require 'lazy'.setup {
         }
       }
     end
+  },
+  {
+    'akinsho/toggleterm.nvim', 
+    version = "*", 
+    config = function()
+      require 'toggleterm'.setup {
+        open_mapping = [[<c-_>]], -- Equivalent to <c-/>
+        direction = 'float',
+        float_opts = {
+          border = 'double',
+          zindex = 999,
+          title_pos = 'center',
+        },
+      }
+    end
   }
 }

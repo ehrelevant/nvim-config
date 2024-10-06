f6af10984663e21455526fcf52ba78535be535a5
Ehren Castillo <ehrencastillo@gmail.com>
2024-10-06 23:56:49 +0800

    feat: add telescope.nvim

--- lua/ehrelevant/plugins.lua
+++ lua/ehrelevant/plugins.lua
@@ -541,4 +541,26 @@ require("lazy").setup({
 			},
 		},
 	},
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		config = function()
+			require("telescope").setup({
+				defaults = {
+					mappings = {
+						i = {
+							["<C-h>"] = "which_key",
+						},
+					},
+				},
+			})
+
+			local builtin = require("telescope.builtin")
+			vim.keymap.set("n", "<leader>ff", builtin.find_files, { desc = "Telescope [F]ind [F]iles" })
+			vim.keymap.set("n", "<leader>fg", builtin.live_grep, { desc = "Telescope Live [G]rep" })
+			vim.keymap.set("n", "<leader>fb", builtin.buffers, { desc = "Telescope [B]uffers" })
+			vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Telescope [H]elp Tags" })
+		end,
+	},
 })

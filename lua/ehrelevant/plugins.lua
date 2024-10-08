7ce25a579774752f2f52dc6a4b3bd44664339809
Ehren Castillo <ehrencastillo@gmail.com>
2024-10-08 23:00:27 +0800

    fix: add Trouble to neo-tree "do not replace" list

--- lua/ehrelevant/plugins.lua
+++ lua/ehrelevant/plugins.lua
@@ -225,7 +225,7 @@ require("lazy").setup({
 
 			require("neo-tree").setup({
 				sources = { "filesystem", "buffers", "git_status" },
-				open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+				open_files_do_not_replace_types = { "terminal", "Trouble", "trouble", "qf" },
 				enable_git_status = true,
 				enable_diagnostics = true,
 				filesystem = {

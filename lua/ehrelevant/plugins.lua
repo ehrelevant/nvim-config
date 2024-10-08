66e247c2c0f0e169247192db0f01575768350cb9
Ehren Castillo <ehrencastillo@gmail.com>
2024-10-09 00:50:05 +0800

    feat: add auto-session

--- lua/ehrelevant/plugins.lua
+++ lua/ehrelevant/plugins.lua
@@ -663,4 +663,38 @@ require("lazy").setup({
 			},
 		},
 	},
+	{
+		"rmagatti/auto-session",
+		lazy = false,
+		keys = {
+			-- Will use Telescope if installed or a vim.ui.select picker otherwise
+			{ "<leader>wr", "<cmd>SessionSearch<CR>", desc = "Session search" },
+			{ "<leader>ws", "<cmd>SessionSave<CR>", desc = "Save session" },
+			{ "<leader>wa", "<cmd>SessionToggleAutoSave<CR>", desc = "Toggle autosave" },
+		},
+		init = function()
+			vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+		end,
+		opts = {
+			suppressed_dirs = { "~/", "~/downloads/", "~/programs", "/" },
+			bypass_save_filetypes = { "neo-tree" },
+			pre_save_cmds = {
+				"Neotree close",
+			},
+			post_restore_cmds = {
+				"Neotree show filesystem",
+			},
+			session_lens = {
+				load_on_setup = true,
+				mappings = {
+					delete_session = { "i", "<C-D>" },
+					alternate_session = { "i", "<C-S>" },
+					copy_session = { "i", "<C-Y>" },
+				},
+				theme_conf = {
+					border = true,
+				},
+			},
+		},
+	},
 })

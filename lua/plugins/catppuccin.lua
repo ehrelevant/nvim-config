883d263fa8384639497b9a7ace01840509a5212e
Ehren Castillo <ehrencastillo@gmail.com>
2024-12-15 14:47:22 +0800

    refactor: prefer no italics

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -2,6 +2,11 @@ return {
 	"catppuccin/nvim",
 	lazy = false,
 	priority = 1000,
+	config = function()
+		require("catppuccin").setup({
+			no_italic = true,
+		})
+	end,
 	init = function()
 		vim.cmd.colorscheme("catppuccin-macchiato")
 	end,

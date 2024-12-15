93e3f353a6022624377468c6723db7babef19c18
Ehren Castillo <ehrencastillo@gmail.com>
2024-12-15 11:53:56 +0800

    feat: add non-working bufferline diagnostics indicators

--- lua/ehrelevant/plugins.lua
+++ lua/ehrelevant/plugins.lua
@@ -362,6 +362,10 @@ require("lazy").setup({
 					close_command = function(bufnum)
 						require("bufdelete").bufdelete(bufnum, true)
 					end,
+					diagnostics_indicator = function(count, level, diagnostics_dict, context)
+						local icon = level:match("error") and " " or " "
+						return " " .. icon .. count
+					end,
 				},
 			})
 		end,

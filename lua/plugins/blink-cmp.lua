ac788daa768031ca60c4620c6cd5a03284d64115
Ehren Castillo <ehrencastillo@gmail.com>
2025-08-04 00:14:25 +0800

    deps: install blink.cmp

---
+++ lua/plugins/blink-cmp.lua
@@ -0,0 +1,19 @@
+return {
+		'saghen/blink.cmp',
+		event = 'VeryLazy',
+		dependencies = {
+				'rafamadriz/friendly-snippets',
+		},
+		version = '1.*',
+
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+				completion = {
+						documentation = {
+								auto_show = true,
+						},
+				},
+		},
+		opts_extend = { 'sources.default' },
+}
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,11 +1,14 @@
 return {
 		'mason-org/mason-lspconfig.nvim',
 		dependencies = {
-				{ 
+				{
 						'mason-org/mason.nvim',
 						event = 'VeryLazy',
 						opts = {},
 				},
 				'neovim/nvim-lspconfig',
+				'saghen/blink.cmp',
 		},
+		opts = {},
 }
+

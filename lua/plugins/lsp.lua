ab33953cdb6dfa86ef7a541aee190ddfcd42018c
Ehren Castillo <ehrencastillo@gmail.com>
2025-08-04 11:34:05 +0800

    deps: install typescript-tools.nvim

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,18 +1,25 @@
 return {
-		'mason-org/mason-lspconfig.nvim',
-		dependencies = {
-				{
-						'mason-org/mason.nvim',
-						event = 'VeryLazy',
-						opts = {},
+		{
+				'mason-org/mason-lspconfig.nvim',
+				dependencies = {
+						{
+								'mason-org/mason.nvim',
+								event = 'VeryLazy',
+								opts = {},
+						},
+						'neovim/nvim-lspconfig',
+						'saghen/blink.cmp',
 				},
-				'neovim/nvim-lspconfig',
-				'saghen/blink.cmp',
+				opts = {},
+				init = function()
+						-- Add border to hover documentation preview
+						vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = 'single' }) end)
+				end
 		},
-		opts = {},
-		init = function()
-				-- Add border to hover documentation preview
-				vim.keymap.set("n", "K", function() vim.lsp.buf.hover({ border = 'single' }) end)
-		end
+		{
+				"pmizio/typescript-tools.nvim",
+				dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+				opts = {},
+		},
 }
 

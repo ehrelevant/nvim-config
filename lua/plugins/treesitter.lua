74d9aacf6b8f68faf796f969e53067f4cbbe32d2
Ehren Castillo <ehrencastillo@gmail.com>
2025-08-08 02:00:39 +0800

    feat(treesitter): add treesitter configuration

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,35 @@
+return {
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+				require("nvim-treesitter.configs").setup({
+						ensure_installed = {
+								"bash",
+								"diff",
+								"c",
+								"lua",
+								"luadoc",
+								"vim",
+								"vimdoc",
+								"markdown",
+								"markdown_inline",
+								"toml",
+								"yaml",
+								"xml",
+								"python",
+								"html",
+								"javascript",
+								"typescript",
+								"jsdoc",
+								"json",
+								"jsonc",
+								"svelte",
+								"julia",
+								"php",
+						},
+						sync_install = false,
+						highlight = { enable = true },
+						indent = { enable = true },
+				})
+		end,
+}
